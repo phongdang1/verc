70f59f2d26c5c62132cd73d09e09d0130609253e
phongdang1 <phongdbde160574@fpt.edu.vn>
2024-10-31 20:56:57 +0700

    dbAdd4

--- server/db.js
+++ server/db.js
@@ -1,12 +1,12 @@
 // db.js
 const mysql = require("mysql2");
-require('dotenv').config(); // Tải các biến môi trường từ file .env
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,      // Lấy từ biến môi trường
-  user: process.env.DB_USER,      // Lấy từ biến môi trường
-  password: process.env.DB_PASSWORD, // Lấy từ biến môi trường
-  database: process.env.DB_NAME    // Lấy từ biến môi trường
+  host: process.env.DB_HOST,      
+  user: process.env.DB_USER,      
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME   
 });
 
 // Kiểm tra kết nối
